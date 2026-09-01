b438ab895248b22d8cfb166a69733f47e2f76629
gana3ah79-debug <gana3ah79@gmail.com>
2026-09-01 15:15:25 +0300

    Configure Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.MADA_SUPABASE_URL='https://eclnddvupggxyythtpkv.supabase.co';
-window.MADA_SUPABASE_KEY='sb_publishable_FqI5heK77syr-3QHh2LPHg_E82vbq-0';
+const SUPABASE_URL = 'https://eclnddvupggxyythtpkv.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'REPLACE_WITH_SUPABASE_PUBLISHABLE_KEY';
