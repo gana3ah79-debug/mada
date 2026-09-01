cdc084eb90a50e98b7c598911f50000c27436c2c
gana3ah79-debug <gana3ah79@gmail.com>
2026-09-01 15:15:36 +0300

    Configure Supabase publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = 'https://eclnddvupggxyythtpkv.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'REPLACE_WITH_SUPABASE_PUBLISHABLE_KEY';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_FqI5heK77syr-3QHh2LPHg_E82vbq-0';
