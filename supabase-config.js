dcc140a581ac73a9f9fb5d4b03b6216513eb8530
gana3ah79-debug <gana3ah79@gmail.com>
2026-09-06 00:22:17 +0300

    Load Reels comments and replies

--- supabase-config.js
+++ supabase-config.js
@@ -13,4 +13,5 @@ window.MADA_SUPABASE_CLIENT = window.supabase.createClient(SUPABASE_URL, SUPABAS
   load('mada-reels-side-actions-v1.js?v20260906-1','data-mada-reels-side-actions');
   load('mada-reels-reaction-dock-v1.js?v20260906-2','data-mada-reels-reaction-dock');
   load('mada-reels-fullscreen-v1.js?v20260906-1','data-mada-reels-fullscreen');
+  load('mada-reels-comments-v1.js?v20260906-1','data-mada-reels-comments');
 })();
