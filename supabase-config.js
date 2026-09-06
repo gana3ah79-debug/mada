77916db1b1040b001428d2a5e3d7d04d963aaa83
gana3ah79-debug <gana3ah79@gmail.com>
2026-09-06 13:46:58 +0300

    Load Messenger read receipts update

--- supabase-config.js
+++ supabase-config.js
@@ -30,7 +30,7 @@ window.MADA_SUPABASE_CLIENT = window.supabase.createClient(SUPABASE_URL, SUPABAS
   load('mada-comments-v1.js?v20260906-1','data-mada-comments-v1');
   load('mada-reels-comments-bridge-v1.js?v20260906-1','data-mada-reels-comments-bridge');
   load('mada-friends-v1.js?v20260906-1','data-mada-friends-v1');
-  load('mada-messenger-v1.js?v20260906-11','data-mada-messenger-v1');
+  load('mada-messenger-v1.js?v20260906-12','data-mada-messenger-v1');
   load('mada-messenger-status-v1.js?v20260906-1','data-mada-messenger-status-v1');
   load('mada-buzz-popup-v1.js?v20260906-2','data-mada-buzz-popup-v1');
   load('mada-profile-photo-edit-v1.js?v20260906-2','data-mada-profile-photo-edit-v1');
