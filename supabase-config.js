be6165487a8a56c895f9cf5f0ef98e721d531a53
gana3ah79-debug <gana3ah79@gmail.com>
2026-09-06 06:22:52 +0300

    Refresh Messenger mobile click-fix cache

--- supabase-config.js
+++ supabase-config.js
@@ -32,6 +32,6 @@ window.MADA_SUPABASE_CLIENT = window.supabase.createClient(SUPABASE_URL, SUPABAS
   load('mada-friends-v1.js?v20260906-1','data-mada-friends-v1');
   load('mada-messages-v2.js?v20260906-1','data-mada-messages-v2');
   load('mada-messenger-v1.js?v20260906-4','data-mada-messenger-v1');
-  load('mada-messenger-click-fix-v1.js?v20260906-1','data-mada-messenger-click-fix-v1');
+  load('mada-messenger-click-fix-v1.js?v20260906-2','data-mada-messenger-click-fix-v1');
   load('mada-profile-photo-edit-v1.js?v20260906-2','data-mada-profile-photo-edit-v1');
 })();
